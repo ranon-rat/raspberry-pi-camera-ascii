f45c61259656539bc5f6cf3bb436287a8dde6830
ranon-rat <temporalsantiagoloco@gmail.com>
2021-03-31 16:03:28 -0600

    finished

--- main.go
+++ main.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"image"
 	_ "image/jpeg"
 	_ "image/png"
 	"io"
-	"os"
 	"time"
+
+	"github.com/dhowden/raspicam"
 )
 
 var letters = []string{}
@@ -30,15 +32,13 @@ func frase(f string) {
 func openThis(f io.Reader) {
 	//this open the image and print the pixels
 	img, _, _ := image.Decode(f)
-	division := 5
-	for y := img.Bounds().Min.Y; y < img.Bounds().Max.Y/division; y++ {
-		for x := img.Bounds().Min.X; x < img.Bounds().Max.X/division; x++ {
+	division := 27
+	limitY,	limitX :=img.Bounds().Max.Y/division,img.Bounds().Max.X/division
+
+	for y := img.Bounds().Min.Y; y < limitY; y++ {
+		for x := img.Bounds().Min.X; x < limitX; x++ {
 			r, g, b, _ := img.At(x*division, y*division).RGBA()
-			color := ((r / 257) + (g / 257) + (b / 257)) / 3
-			for int(color) > len(letters)-1 {
-				color--
-			}
-			fmt.Print(letters[color])
+			fmt.Print(letters[int(((r / 257) + (g / 257) + (b / 257)) / 3) % len(letters)]+" ")
 		}
 		fmt.Println()
 
@@ -47,31 +47,17 @@ func openThis(f io.Reader) {
 
 func main() {
 	addthis()
-	when, _ := os.Open("image/when.jpeg")
-	defer when.Close()
-	//// cuando you make your memes in videos
-	frase("when haces tus momos en consola")
-	fmt.Println()
-	/// file open when
-	openThis(when)
-	futuro, _ := os.Open("image/futuro.jpg")
-	defer futuro.Close()
-	/////// the future is today you listen,old man?
-	frase("el futuro es hoy ¿Oiste viejo?")
-	openThis(futuro)
-	baneando, _ := os.Open("image/baneando.jpg")
-	defer baneando.Close()
-	// but they band you
-	frase("pero te terminan baneando")
-	openThis(baneando)
-	jaja, _ := os.Open("image/jajaja.jpeg")
-	defer jaja.Close()
-	//oh my contact eye ahahhahahahhah
-	frase("oh! mi lente de contacto jajajajajajj")
-	openThis(jaja)
-	gopher, _ := os.Open("image/gopher.png")
-	defer gopher.Close()
-	frase("momos en golang")
-	openThis(gopher)
+	s := raspicam.NewStill()
+	for {
+		buffer := new(bytes.Buffer)
+		errCh := make(chan error)
+		/*go func() {
+			for x := range errCh {
+				fmt.Fprintf(os.Stderr, "%v\n", x)
+			}
+		}()*/
+		raspicam.Capture(s, buffer, errCh)
+		go openThis(buffer)
+	}
 
 }
